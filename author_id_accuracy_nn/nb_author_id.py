1174604b7a0172561a446eb69245a8a831e368f1
Lakshadeep <lakshadeep.naik@gmail.com>
2016-07-17 23:44:42 +0530

    * author id accuracy using nearby neighbours

---
+++ author_id_accuracy_nn/nb_author_id.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+
+""" 
+    This is the code to accompany the Lesson 1 (Naive Bayes) mini-project. 
+
+    Use a Naive Bayes Classifier to identify emails by their authors
+    
+    authors and labels:
+    Sara has label 0
+    Chris has label 1
+"""
+    
+import sys
+from time import time
+from email_preprocess import preprocess
+
+
+### features_train and features_test are the features for the training
+### and testing datasets, respectively
+### labels_train and labels_test are the corresponding item labels
+features_train, features_test, labels_train, labels_test = preprocess()
+
+
+# print features_test
+
+#########################################################
+### your code goes here ###
+from sklearn import neighbors
+n_neighbors = 15
+clf = neighbors.KNeighborsClassifier(n_neighbors)  
+t0 = time()
+classifier = clf.fit(features_train, labels_train)
+print "Training Time:", round(time()-t0, 3), "s"
+
+t0 = time()
+output = clf.predict(features_test)
+print "Prediction Time:", round(time()-t0, 3), "s"
+# print output
+
+wrong_count = 0
+for i in range(len(output)):
+	if(output[i] != labels_test[i]):
+		wrong_count = wrong_count + 1
+
+print "Accuracy: " + str((1 - wrong_count / 250.0) * 100)
+
+#########################################################
+
+
+
+
